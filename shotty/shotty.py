96f4a27993812866046606078ad29389c1ab97b8
Tavas  Neuwirth <tavasneuwirth@Tavass-MacBook-Pro.local>
2020-09-08 13:49:13 -0600

    still working on ec2

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,12 +1,35 @@
 import boto3
+import click
 
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-def list_instances():
-    for i in ec2.instances.all():
-        print(i)
 
+@click.command()
+@click.option('--project', default=None,
+    help="Only instance for project (tag Project: <name>)")
+
+def list_instances():
+    "List EC2 instances"
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or []}
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
+            )))
+    return
 
 if __name__ == '__main__':
     list_instances()
