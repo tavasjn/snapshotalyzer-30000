63143d613f088073ff1a5fc352a22d25c7c23bec
Tavas  Neuwirth <tavasneuwirth@Tavass-MacBook-Pro.local>
2020-09-09 09:21:02 -0600

    working on ec2

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -7,20 +7,19 @@ ec2 = session.resource('ec2')
 
 @click.command()
 @click.option('--project', default=None,
-    help="Only instance for project (tag Project: <name>)")
-
+              help="Only instance for project (tag Project: <name>)")
 def list_instances():
     "List EC2 instances"
     instances = []
 
     if project:
-        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
         instances = ec2.instances.filter(Filters=filters)
     else:
         instances = ec2.instances.all()
 
     for i in instances:
-        tags = { t['Key']: t['Value'] for t in i.tags or []}
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(', '.join((
             i.id,
             i.instance_type,
@@ -28,8 +27,28 @@ def list_instances():
             i.state['Name'],
             i.public_dns_name,
             tags.get('Project', '<no project>')
-            )))
+        )))
     return
+
+
+@instances.command('stop')
+@click.option('--project', default=None, help='Only instances for project')
+def stop_instances(project):
+    "Stop EC2 instances"
+
+    instances = []
+
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+    return
+
 
 if __name__ == '__main__':
     list_instances()
